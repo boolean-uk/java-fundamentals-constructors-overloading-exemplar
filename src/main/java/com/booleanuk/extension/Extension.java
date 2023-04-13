7177770c7135c3fc5e10265e916d02ce4ff5e695
Dave Ames <david.john.ames@gmail.com>
2023-04-13 16:57:40 +0100

    Solutions for the extension exercise

--- src/main/java/com/booleanuk/extension/Extension.java
+++ src/main/java/com/booleanuk/extension/Extension.java
@@ -3,28 +3,60 @@ package com.booleanuk.extension;
 import com.booleanuk.helpers.ExtensionBase;
 
 public class Extension extends ExtensionBase {
-    /*
-        Implement the following methods:
-
-        1. add, which accepts two floats and returns a float (both floats added together)
-
-        2. add, which accepts two doubles and returns a double (both doubles added together)
-
-        3. subtract, which accepts two floats and returns a float (first float minus second float)
-
-        4. subtract, which accepts a String and a char and returns a string with all instances of the provided char removed
-
-        5. multiply, which accepts two ints and returns an int (first int multiplied by second int)
-
-        6. multiply, which accepts a string and an int, and returns a string containing the provided string
-        as many times as the provided int separated by a comma. E.g.
-        multiply("Hello", 3) -> "Hello,Hello,Hello"
-
-        7. multiply, which accepts an array of Strings that each contain a number, and an int
-        The method should return an array of ints that contain the value of multiplying each String number by the provided int
-        E.g.
-        multiply(["2", "7", "3"], 3) -> [6, 21, 9]
-     */
-
+//        Implement the following methods:
+//
+//        1. add, which accepts two floats and returns a float (both floats added together)
+    public float add(float num1, float num2) {
+        return num1 + num2;
+    }
+//        2. add, which accepts two doubles and returns a double (both doubles added together)
+    public double add(double num1, double num2) {
+        return num1 + num2;
+    }
+//        3. subtract, which accepts two floats and returns a float (first float minus second float)
+    public float subtract(float num1, float num2) {
+        return num1 - num2;
+    }
+//
+//        4. subtract, which accepts a String and a char and returns a string with all instances of the provided char removed
+    public String subtract(String first, char toRemove) {
+        String result = "";
+        for (int i = 0; i < first.length(); i++) {
+            if (first.charAt(i) != toRemove) {
+                result += first.charAt(i);
+            }
+        }
+        return result;
+    }
+//
+//        5. multiply, which accepts two ints and returns an int (first int multiplied by second int)
+    public int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+//
+//        6. multiply, which accepts a string and an int, and returns a string containing the provided string
+//        as many times as the provided int separated by a comma. E.g.
+//        multiply("Hello", 3) -> "Hello,Hello,Hello"
+    public String multiply(String inwards, int multiplier) {
+        String result = "";
+        for (int i = 0; i < multiplier; i++) {
+            result += inwards + ",";
+        }
+        result = result.substring(0, result.length() -1);
+        return result;
+    }
+//
+//        7. multiply, which accepts an array of Strings that each contain a number, and an int
+//        The method should return an array of ints that contain the value of multiplying each String number by the provided int
+//        E.g.
+//        multiply(["2", "7", "3"], 3) -> [6, 21, 9]
+    public int[] multiply(String[] numbers, int multiplier) {
+        int[] results = new int[numbers.length];
+        for (int i = 0; i < numbers.length; i++){
+            int x = Integer.parseInt(numbers[i]);
+            results[i] = x * multiplier;
+        }
+        return results;
+    }
 
 }
